edf181a608d274167e6d3b6095ad244ca0ae1ada
felipetanello <132622091+felipetanello@users.noreply.github.com>
2024-05-13 21:24:28 -0300

    Add files via upload

---
+++ Jogador.java
@@ -0,0 +1,16 @@
+package br.com.unicuritiba.dama.model;
+
+public class Jogador {
+
+	// atributo
+	private String nome;
+
+	// getters and setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
